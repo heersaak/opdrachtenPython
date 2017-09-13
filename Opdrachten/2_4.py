077c8cfc5e56967f6dc33782e9f01be738cc796f
Rik <rik.denhartog@student.hu.nl>
2017-09-13 14:30:46 +0200

    School opdrachten 13-09-2017

---
+++ Opdrachten/2_4.py
@@ -0,0 +1,6 @@
+##Schrijf een programma dat de gebruiker vraagt om zijn uurloon, het aantal uur dat hij of zij gewerkt
+##heeft en dat daarna het salaris uitprint.
+
+uren = float(input('Hoeveel uren heb je gemaakt? ' ))
+loon = float(input('Hoeveel verdien je per uur?'))
+print(uren,'uur werken levert',(uren*loon),'euro op')
