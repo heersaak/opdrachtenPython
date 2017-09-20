778389420f4428718964b75cdc664763e725b8c2
Rik <rik.denhartog@student.hu.nl>
2017-09-20 16:46:44 +0200

    School opdrachten

--- Opdrachten/4_3.py
+++ Opdrachten/4_3.py
@@ -1,7 +1,7 @@
 ## Schrijf (en test) de functie lang_genoeg() die voor Efteling-attracties bepaalt of een gebruiker in een
 ## attractie mag. De functie heeft één parameter, namelijk de lengte van de gebruiker in centimeters. Als
 ## de gebruiker 120 centimeter of langer is de return-waarde van de functie “Je bent lang genoeg voor
-## de attractie!”, anders is de melding “Sorry, je bent te klein!”.
+## de attractie!”, anders is de melding “Sorry, je bent te klein!”. 
 
 def lang_genoeg(lengte):
     if lengte >= 120:
