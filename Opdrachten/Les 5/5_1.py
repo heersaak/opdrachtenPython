e235f3b8b1dc8617ae27b1931159a4f60219cdc1
Rik den Hartog <rik.denhartog@student.hu.nl>
2017-09-28 17:02:09 +0200

    School opdrachten

---
+++ Opdrachten/Les 5/5_1.py
@@ -0,0 +1,20 @@
+##Schrijf een functie convert() waar je een temperatuur in graden Celsius (als parameter van deze
+##functie) kunt omzetten naar graden Fahrenheit. Je kunt de temperatuur in Fahrenheit berekenen met
+##de formule T(°F) = T(°C) × 1.8 + 32. Dus 25 °C = 25 * 1.8 + 32 = 77 °F.
+
+##Schrijf nu ook een tweede functie table() waarin je met een for-loop van -30 °C t/m 40 °C in stappen
+##van 10 graden de temperatuur in Fahrenheit print. Zorg middels een geformatteerde output voor
+##dezelfde precisie en uitlijning als het voorbeeld hieronder:
+
+def convert(Celcius):
+    fah = Celcius * 1.8 +32
+    return fah
+
+def table():
+    print(' {:7} {}'.format("F", "X"))
+    for x in range(-30, 50, 10):
+        print( '{:5} {:5}'.format(convert(x), x))
+
+print(table())
+
+
---
+++ Opdrachten/Les 5/5_2.py
@@ -0,0 +1,15 @@
+##Schrijf een Python programma waarmee je het bestand opent, en splits elke regel op de komma en
+##zorg dat de uitvoer (op het scherm) is zoals op de volgende pagina is weergegeven! Vergeet niet het
+##bestand te sluiten!
+
+infile = open('kaartnummers.txt', 'r')
+text = infile.readlines()
+
+def split():
+    for i in text:
+        a,b=i.split(",")
+        c = b.replace('\n', '')
+        print(c, 'Heeft kaartnummer:', a)
+
+infile.close()
+print(split())
